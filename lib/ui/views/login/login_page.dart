844bb22bfc3880118a6374a820ec2b214dc368b3
User <user@Juninho-MacBook-Pro.local>
2024-09-20 20:30:45 -0300

    teste merge

--- lib/ui/views/login/login_page.dart
+++ lib/ui/views/login/login_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+class LoginPageTest extends StatelessWidget {
+  const LoginPageTest({super.key});
 
   @override
   Widget build(BuildContext context) {
